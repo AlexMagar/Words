43be759058502cc040e30a7a515d4f5cb5fe20a5
AlexMagar <lx.mgr07@gmail.com>
2024-02-13 22:31:44 +1100

    feat: menubar created

---
+++ src/Components/layout/Header.js
@@ -0,0 +1,27 @@
+import { AppBar, Button, IconButton, Toolbar, Typography } from '@mui/material'
+import MenuIcon from '@mui/icons-material/Menu'
+import React from 'react'
+import { ForkLeft } from '@mui/icons-material'
+
+export const Header = () => {
+  return (
+    <React.Fragment>
+        <AppBar position='fixed'>
+            <Toolbar>
+                <IconButton 
+                    size='large'
+                    edge='start'
+                    color='inherit'
+                    aria-label='menu'
+                    sx={{ mr: 2}}
+                >
+                    <MenuIcon/>
+                </IconButton>
+                <Typography variant='h5' component='div' sx={{ flexGrow: 1, textAlign:'left'}} >Words</Typography>
+                <Button color='inherit'>SignIn</Button>
+                <Button color='inherit'>Register</Button>
+            </Toolbar>
+        </AppBar>
+    </React.Fragment>
+  )
+}
