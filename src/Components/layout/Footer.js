6f765ca65cc9de4f4ef2944c50c356404524e931
AlexMagar <lx.mgr07@gmail.com>
2024-02-15 23:25:08 +1100

    chore: styling footer

--- src/Components/layout/Footer.js
+++ src/Components/layout/Footer.js
@@ -1,9 +1,20 @@
+import { AppBar, Toolbar } from '@mui/material'
 import React from 'react'
 
 export const Footer = () => {
   return (
     <React.Fragment>
-      &copy; copy right all reserved || Words
+      <Toolbar 
+        sx={{
+          position: 'static',
+          backgroundColor: '#525252',
+          justifyContent: 'center',
+          height: '8vh',
+          color: '#fff'
+        }}
+      >
+        &copy; copy right all reserved || Words
+      </Toolbar>
     </React.Fragment>
   )
 }
