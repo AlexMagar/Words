b68496f4f40aa936440cc055e6eccccec39a362d
AlexMagar <lx.mgr07@gmail.com>
2024-02-15 23:26:26 +1100

    chore: card component imported and header component into div

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import './App.css';
+import { Card } from './Components/admin-signup/Card';
 import { Footer } from './Components/layout/Footer';
 import { Header } from './Components/layout/Header';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <div className="App-header">
         <Header />
+        <Card />
         <Footer />
-      </header>
+      </div>
     </div>
   );
 }
